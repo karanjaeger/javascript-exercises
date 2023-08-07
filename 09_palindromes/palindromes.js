a2836b7aae0fe73b508c62e61a93809a2517b9cd
KaranYaeger <sharath.g1@byjus.com>
2023-08-07 09:00:29 +0530

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) 
+{   
+    const newString = string.replace(/[^\w\s]/g, '').replace(/\s+/g, '').toLowerCase();
+    return newString === newString.split("").reverse().join("");
 };
 
 // Do not edit below this line
