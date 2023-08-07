92129fb60ab3d6cc6ee34b24adce103f478c06e9
KaranYaeger <sharath.g1@byjus.com>
2023-08-07 12:42:32 +0530

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) 
+{
+    if (num > 0) 
+    {
+        let fArray = [0,1];
+        for (let i = 2; i < 27; i++ ) 
+        {
+            fArray[i] = fArray[i-1] + fArray[i-2]
+        }
+        return fArray[num];        
+    }
+    else return "OOPS"
 };
 
 // Do not edit below this line
