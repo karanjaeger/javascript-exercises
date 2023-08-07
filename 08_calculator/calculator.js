e47653aa573cbac9ddcd49e23905bd936f1036a6
KaranYaeger <sharath.g1@byjus.com>
2023-08-07 08:23:40 +0530

    Complete exericese 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i = 0; i < array.length; i++) 
+  {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let prod = 1;
+	for (let i = 0; i < array.length; i++) 
+  {
+    prod *= array[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let exponent = 1;
+  for(let i = 0; i < power; i++){
+    exponent = num * exponent;
+  }
+  return exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for(let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
